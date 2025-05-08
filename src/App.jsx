6417031d19b73d5c508d1885859391e643ff2469
giulibrumatti <giulibrumatti97@gmail.com>
2025-05-08 14:14:38 -0300

    nav: add employees

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+import { Route, Routes } from "react-router-dom";
+import AddEmployees from "./components/AddEmployees";
 import ListEmployees from "./components/ListEmployees";
 import NavbarS from "./components/NavbarS";
-import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <NavbarS />
       <Routes>
         <Route path="/empleados" element={<ListEmployees />} />
+        <Route path="/agregar" element={<AddEmployees />} />
       </Routes>
     </>
   );
--- src/components/NavbarS.jsx
+++ src/components/NavbarS.jsx
@@ -1,76 +1,50 @@
-import React from "react";
 import {
   Navbar,
-  Collapse,
-  Typography,
-  IconButton,
+  Typography
 } from "@material-tailwind/react";
+import React from "react";
 
-import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
+const nav = [
+  {
+    name: "Inicio",
+    link: "/",
+  },
+  {
+    name: "Empleados",
+    link: "/empleados",
+  },
+  {
+    name: "Agregar",
+    link: "/agregar",
+  },
+];
 function NavList() {
   return (
     <ul className="my-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-medium"
-      >
-        <Link
-          className="flex items-center hover:text-blue-500 transition-colors"
-          to="/empleados"
+      {nav.map((main) => (
+        <Typography
+          as="li"
+          variant="small"
+          color="blue-gray"
+          className="p-1 font-medium"
         >
-          Empleados
-        </Link>
-      </Typography>
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-medium"
-      >
-        <a
-          href="#"
-          className="flex items-center hover:text-blue-500 transition-colors"
-        >
-          Account
-        </a>
-      </Typography>
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-medium"
-      >
-        <a
-          href="#"
-          className="flex items-center hover:text-blue-500 transition-colors"
-        >
-          Blocks
-        </a>
-      </Typography>
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-medium"
-      >
-        <a
-          href="#"
-          className="flex items-center hover:text-blue-500 transition-colors"
-        >
-          Docs
-        </a>
-      </Typography>
+          <Link
+            className="flex items-center hover:text-blue-500 transition-colors"
+            to={main.link}
+          >
+            {main.name}
+          </Link>
+        </Typography>
+      ))}
     </ul>
   );
 }
 
 export default function NavbarS() {
   return (
-    <Navbar className="mx-auto max-w-screen-xl px-6 py-3 text-gray-900">
+    <Navbar className="mx-auto max-w-screen-xl px-6 py-3 text-gray-900 bg-slate-200 mt-4 rounded">
       <div className="flex items-center justify-between text-blue-gray-900">
         <Typography
           as="a"
